5381516f580c6ff9e9a64df26646fd4170235ddd
micaelenn <mirandamicaelen@gmail.com>
2025-01-27 18:05:16 -0300

    style: remove 'utils' directory

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ import { program } from "commander";
 import inquirer from "inquirer";
 
 // internal modules
-import copyAndPastFolder from "./utils/actions.js";
-import { getFoldersName } from "./utils/helpers.js";
+import copyAndPastFolder from "./actions.js";
+import { getFoldersName } from "./helpers.js";
 
 program.version("1.0.0").description("Boiler CLI");
 const boilerplates = getFoldersName('starters')
